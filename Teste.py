fff5e46004577b17420d23df0e2fb39cdc87e61c
Guido Mandelert <guidomandelert@gmail.com>
2023-05-18 15:01:00 -0300

    Tela

--- Teste.py
+++ Teste.py
@@ -1,1 +1,10 @@
-print('vinicius infelizmente os testes voltaram, você tem aids')
+import pygame
+import random
+
+pygame.init()
+pygame.mixer.init()
+
+WIDTH = 480
+HEIGHT = 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Cayman')
