02aac4949cdaab6e6197e498444654014c00f6ac
Guido Mandelert <guidomandelert@gmail.com>
2023-05-18 15:22:01 -0300

    oi

--- Teste.py
+++ Teste.py
@@ -8,5 +8,4 @@ WIDTH = 480
 HEIGHT = 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Cayman')
-bg_img = pygame.image.load("Mega desoft/base.webp")
-bg_img = pygame.transform.scale(bg_img,(WIDTH,HEIGHT))
+background= pygame.image.load("pygame.gv/imagens/base.webp").convert()
