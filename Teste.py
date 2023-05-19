311906297bf6e11eb641c062dede2bf46a15f0d6
viniciusbp7 <130686889+viniciusbp7@users.noreply.github.com>
2023-05-19 15:24:08 -0300

    pastas

--- Teste.py
+++ Teste.py
@@ -8,7 +8,8 @@ pygame.init()
 # ----- Gera tela principal
 WIDTH = 500
 HEIGHT = 400
-window = pygame.display.set_mode((WIDTH, HEIGHT))
+#window = pygame.display.set_mode((WIDTH, HEIGHT))
+window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption('Hello World!')
 
 # ----- Inicia estruturas de dados
@@ -16,6 +17,8 @@ game = True
 
 # ----- Inicia assets
 image = pygame.image.load('imagens/base.webp').convert()
+ritsu= pygame.image.load("imagens/General_Ritsu_Base.png")
+
 
 # ===== Loop principal =====
 while game:
@@ -28,6 +31,7 @@ while game:
     # ----- Gera saídas
     window.fill((0, 0, 0))  # Preenche com a cor branca
     window.blit(image, (10, 10))
+    window.blit(ritsu, (2, 2))
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
